87294ea8fb0040f17f76a84cf29142799e3756ae
keisukekodama <myzk.kodama@gmail.com>
2021-04-02 19:26:08 +0900

    addnicknamepatern

--- main.rb
+++ main.rb
@@ -697,31 +697,51 @@ def sign_up_retry
     input_sign_up_method(@nickname, @email, @password, @first_name, @last_name, @first_name_kana, @last_name_kana)
     @d.find_element(:class,"register-red-btn").click
     @wait.until {@d.find_element(:class,"furima-icon").displayed? rescue false || @d.find_element(:class,"second-logo").displayed? rescue false || /商品の情報を入力/ .match(@d.page_source)}
-
-    if /FURIMAが選ばれる3つの理由/ .match(@d.page_source)
-      @puts_num_array[1][2] = "[1-002] ◯"  #：ニックネームが必須であること"
-      @puts_num_array[1][3] = "[1-003] ◯"  #：メールアドレスが必須である"
-      @puts_num_array[1][4] = "[1-004] ◯"  #：メールアドレスは一意性である"  #これはまだ立証できない
-      @puts_num_array[1][5] = "[1-005] ◯"  #：メールアドレスは@を含む必要がある"  #これはまだ立証できない
-      @puts_num_array[1][6] = "[1-006] ◯"  #：パスワードが必須である"
-      # puts "[1-] ◯：パスワードは6文字以上である"  #これはまだ立証できない
-      # puts "[1-] ◯：パスワードは半角英数字混合である"  #これはまだ立証できない
-      @puts_num_array[1][7] = "[1-007] ◯"  #：パスワードは確認用を含めて2回入力する"  #これはまだ立証できない
-      @puts_num_array[0].push("【補足情報】◯：ユーザー新規登録時にパスワードに大文字を入れたことで登録が成功 (パスワードをaaa111 → Aaa111に変更して登録完了)")
-      @puts_num_array[1][8] = "[1-008] ◯"  #：ユーザー本名が、名字と名前がそれぞれ必須である"  #これはまだ立証できない
-      @puts_num_array[1][9] = "[1-009] ◯"  #：ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させる"  #これはまだ立証できない
-      @puts_num_array[1][10] = "[1-010] ◯"  #：ユーザー本名のフリガナが、名字と名前でそれぞれ必須である"
-      @puts_num_array[1][11] = "[1-011] ◯"  #：ユーザー本名のフリガナは全角（カタカナ）で入力させる"
-      @puts_num_array[1][12] = "[1-012] ◯"  #：生年月日が必須である"  #これはまだ立証できない
-      @puts_num_array[1][13] = "[1-013] ◯"  #：必須項目を入力し、ユーザー登録ができる"
-
-      # パスワードの上書きでも登録が成功しない場合は処理を終了
-    else
-      @puts_num_array[1][13] = "[1-013] ×：必須項目を入力してもユーザー登録ができない"
-      @puts_num_array[0].push("ユーザー登録バリデーションが複雑なためユーザー登録ができません。ユーザー登録できない場合、以降の自動チェックにて不備が発生するため自動チェック処理を終了します")
-      @puts_num_array[0].push("手動でのアプリチェックを行ってください")
-      raise "ユーザー登録バリデーションにて不備あり"
+    # 登録に失敗した場合はニックネームを疑う
+    if /会員情報入力/ .match(@d.page_source
+      # ニックネームの内容でエラーになった可能性あるため、
+      @nickname = "ライフコーチテストユーザーイチ"
+      @nickname2 = "ライフコーチテストユーザー二"
+      @nickname3 = "ライフコーチテストユーザーサン"
+      clear_sign_up_method
+      input_sign_up_method(@nickname, @email, @password, @first_name, @last_name, @first_name_kana, @last_name_kana)
+      @d.find_element(:class,"register-red-btn").click
+      @wait.until {@d.find_element(:class,"furima-icon").displayed? rescue false || @d.find_element(:class,"second-logo").displayed? rescue false || /商品の情報を入力/ .match(@d.page_source)}
+      if /会員情報入力/ .match(@d.page_source)
+        # ニックネームの内容でエラーになった可能性あるため、
+        @nickname = "らいふこーちてすとゆーざーいち"
+        @nickname2 = "らいふこーちてすとゆーざーに"
+        @nickname3 = "らいふこーちてすとゆーざーさん"
+        clear_sign_up_method
+        input_sign_up_method(@nickname, @email, @password, @first_name, @last_name, @first_name_kana, @last_name_kana)
+        @d.find_element(:class,"register-red-btn").click
+        @wait.until {@d.find_element(:class,"furima-icon").displayed? rescue false || @d.find_element(:class,"second-logo").displayed? rescue false || /商品の情報を入力/ .match(@d.page_source)}
+      end
     end
+  end
+
+  if /FURIMAが選ばれる3つの理由/ .match(@d.page_source)
+    @puts_num_array[1][2] = "[1-002] ◯"  #：ニックネームが必須であること"
+    @puts_num_array[1][3] = "[1-003] ◯"  #：メールアドレスが必須である"
+    @puts_num_array[1][4] = "[1-004] ◯"  #：メールアドレスは一意性である"  #これはまだ立証できない
+    @puts_num_array[1][5] = "[1-005] ◯"  #：メールアドレスは@を含む必要がある"  #これはまだ立証できない
+    @puts_num_array[1][6] = "[1-006] ◯"  #：パスワードが必須である"
+    # puts "[1-] ◯：パスワードは6文字以上である"  #これはまだ立証できない
+    # puts "[1-] ◯：パスワードは半角英数字混合である"  #これはまだ立証できない
+    @puts_num_array[1][7] = "[1-007] ◯"  #：パスワードは確認用を含めて2回入力する"  #これはまだ立証できない
+    @puts_num_array[0].push("【補足情報】◯：ユーザー新規登録時にパスワードに大文字を入れたことで登録が成功 (パスワードをaaa111 → Aaa111に変更して登録完了)")
+    @puts_num_array[1][8] = "[1-008] ◯"  #：ユーザー本名が、名字と名前がそれぞれ必須である"  #これはまだ立証できない
+    @puts_num_array[1][9] = "[1-009] ◯"  #：ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させる"  #これはまだ立証できない
+    @puts_num_array[1][10] = "[1-010] ◯"  #：ユーザー本名のフリガナが、名字と名前でそれぞれ必須である"
+    @puts_num_array[1][11] = "[1-011] ◯"  #：ユーザー本名のフリガナは全角（カタカナ）で入力させる"
+    @puts_num_array[1][12] = "[1-012] ◯"  #：生年月日が必須である"  #これはまだ立証できない
+    @puts_num_array[1][13] = "[1-013] ◯"  #：必須項目を入力し、ユーザー登録ができる"
+    # パスワードの上書きでも登録が成功しない場合は処理を終了
+  else
+    @puts_num_array[1][13] = "[1-013] ×：必須項目を入力してもユーザー登録ができない"
+    @puts_num_array[0].push("ユーザー登録バリデーションが複雑なためユーザー登録ができません。ユーザー登録できない場合、以降の自動チェックにて不備が発生するため自動チェック処理を終了します")
+    @puts_num_array[0].push("手動でのアプリチェックを行ってください")
+    raise "ユーザー登録バリデーションにて不備あり"
   end
 end
 
